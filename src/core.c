ba5bed6dfcd4c4b953d06b5d3646737edb23d453
TheIttyBittyGalaxy <43608508+TheIttyBittyGalaxy@users.noreply.github.com>
2025-09-27 16:52:25 +0100

    Additional core functions

--- src/core.c
+++ src/core.c
@@ -5,3 +5,14 @@ int mod(int a, int b)
     int r = a % b;
     return r >= 0 ? r : r + b;
 }
+
+int max2(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+int max3(int a, int b, int c)
+{
+    int x = max2(a, b);
+    return max2(x, c);
+}
--- src/core.h
+++ src/core.h
@@ -2,13 +2,17 @@
 
 #include <assert.h>
 #include <float.h>
+#include <limits.h>
 #include <math.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define UNREACHABLE __builtin_unreachable();
 
 int mod(int a, int b);
+
+int max2(int a, int b);
+int max3(int a, int b, int c);
